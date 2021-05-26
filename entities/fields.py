7efbfde9c656ed43de85909f4b90cae1eb90de0e
ivanuria <ivan.uria@gmail.com>
2021-05-26 10:46:27 +0200

    added "table" to Field definition

--- entities/fields.py
+++ entities/fields.py
@@ -3,11 +3,12 @@ from collections import defaultdict
 
 
 class Field:
-
-    def __init__(self, name, definition, description=""):
+    persistent = defaultdict(dict)
+    def __init__(self, table, name, definition, description=""):
         self._name = name
         self._definition = definition
         self.description = description
+        self._table = table
 
     @property
     def name(self):
@@ -17,11 +18,15 @@ class Field:
     def definition(self):
         return self._definition
 
+    @property
+    def table(self):
+        return self._table
+
 class Fields(dict):
     def __init__(self, table, fields):
         super().__init__(self)
         if isinstance(fields, dict):
-            self._fields = list(map(lambda x: Field(x, fields[x]), fields))
+            self._fields = list(map(lambda x: Field(table, x, fields[x]), fields))
         elif isinstance(fields, list) and all([isinstance(field, Field) for field in fields]):
             self._fields = Fields
         for field in self._fields:
