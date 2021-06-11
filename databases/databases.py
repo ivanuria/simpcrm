e7650d24baf560b30d05d92085817702338cf755
ivanuria <ivan.uria@gmail.com>
2021-06-11 19:36:40 +0200

    Beggining documentation

--- databases/databases.py
+++ databases/databases.py
@@ -1,8 +1,33 @@
-# databases.py
+#!/usr/bin/env python
+
+__author__ = "Iván Uría"
+
+"""
+databases.py gives an interface "DBInterface" to inherit from to develop any kind
+of database interface
+"""
 
 from enum import Enum, auto
 
 class DBEnums(Enum):
+    """Enumerator of Constants used by database objects.
+
+    Attributes:
+        SELECT: Indicator for select clauses in sql or any database retreving data
+        INSERT: Indicator for insert clauses in sql or any database insertion data
+        UPDATE: Indicator for update clauses in sql or any database replacing data
+        DELETE: Indicator for delete clauses in sql or any database deleting data
+        CREATE_TABLE: Indicator for table creation in sql or any database tree creation
+        CREATE_TABLE_AS_ANOTHER: Indicator for table creation and copying data from another one
+        DROP_TABLE: Indicator for table deletion in sql or any database tree deletion
+        ALTER_TABLE_ADD_COLUMN: Indicator for adding a column in sql
+        ALTER_TABLE_DROP_COLUMN: Indicator for deleting a column in sql
+        ALTER_TABLE_RENAME_TABLE: Indicator for renaming a table in sql or any tree renaming
+        ALTER_TABLE_RENAME_COLUMN: Indicator for renaming a column in sql
+        ALTER_TABLE_MODIFY_COLUMN: Indicator for changing type of a column in sql
+        GET_SCHEMA: Indicator for getting the schema in sql
+        PRIMARY: Indicator for adding a column in sql
+    """
     SELECT = auto()
     INSERT = auto()
     UPDATE = auto()
@@ -19,13 +44,56 @@ class DBEnums(Enum):
     CREATE_TABLE_AS_ANOTHER = auto()
 
 class Data(list): #not checked datatypes
+    """Custom list inherited class to check if it has been checked or not
+    """
     def __init__(self, data):
         if isinstance(data, dict):
             data = [data]
         super().__init__(data)
 
 class DBInterface:
-    def __init__(self, database="", server="localhost", user="", password="", encription=""):
+    """Semi-abstract class to derive implementations to access any kind of databases
+
+    Attributes:
+        database: name of the database
+        server: server path
+        table: active table or tree
+        filter: active filter
+        sql_dict: default dictionary to pass to use in inner methods with relevant
+            information. Defaults are:
+                {"table": self.table,
+                 "filter": self.filter,
+                 "method": DBEnums.SELECT,
+                 "fields": [],
+                 "data": [],
+                 "exists":True
+                 }
+    Arguments:
+        database: (str) name of the database
+        server: (str) server path
+        user: (str)  user name if required
+        password: (str) password if required
+        encryption: (str) if exncryption is required -> implementation not defined yet
+    Methods:
+        connect: connects to database with instance defined attributes.
+            Must be overriden
+        disconnect: disconnects from database. Must be overriden
+        set_database: sets database attribute to indicated argument
+        set_table: sets table attribute to indicated table name
+        set_filter:  sets filter attribute to indicated filter dictionary
+        check_table_exists: check if defined table exists in database
+        delete_database: deletes defiend database. Must be overriden
+        _create_sql_query: creates an sql query if needed. Must be overriden
+        create_table: creates indicated table
+        drop_table: deletes indicated table. Must be called from super() on overriding
+        select: gets and returns data from table. Must be called from super() on overriding
+        insert: inserts data on table. Must be called from super() on overriding
+        update: updates data from table with indicated filtera.
+            Must be called from super() on overriding
+        delete: deletes data from table with indicated filtera.
+            Must be called from super() on overriding
+    """
+    def __init__(self, database="", server="localhost", user="", password="", encryption=""):
         """
         Initializes DB Interface
         KWARGS:
@@ -33,13 +101,13 @@ class DBInterface:
             database -> db name
             user -> user name to access server
             password -> password to access server
-            encription -> if encription is needed
+            encryption -> if encription is needed
         """
         self._database = database
         self._server = server
         self._user = user
         self._password = password
-        self._encription = encription
+        self._encryption = encription
         self._table = ""
         self._filter = {}
 
@@ -121,18 +189,20 @@ class DBInterface:
         """
         disconnects and deletes selectted database
         """
-        self.disconnect()
+        raise NotImplementedError
 
     def _create_sql_query(cls, **kwargs):
         """
         creates sql query with given args
           kwargs may have: "table", "method", "fields", "data"
         """
+        raise NotImplementedError
 
     def create_table(self, table, fields={}):
         """
         creates table with fields definition
         """
+        raise NotImplementedError
 
     def drop_table(self, database=None, table=None):
         """
