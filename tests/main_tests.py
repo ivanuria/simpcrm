fdaa6eec66f0e01d72a8f7ab661dde468bc753f8
ivanuria <ivan.uria@gmail.com>
2021-06-21 18:25:20 +0200

    Plotted main_test

---
+++ tests/main_tests.py
@@ -0,0 +1,11 @@
+# main_tests.py
+# Unit Testing for databases
+
+VERSION = 0.1
+
+import unittest
+from interface import main
+
+class v1_Main(unittest.TestCase):
+    def setUp(self):
+        self.main = Main("./test_main_config.ini")
