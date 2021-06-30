adba12639e6cb5686defec549780b87a180ab864
ivanuria <ivan.uria@gmail.com>
2021-06-30 22:25:17 +0200

    Documented

--- entities/defaults.py
+++ entities/defaults.py
@@ -1,11 +1,29 @@
-#Default entities
+#!/usr/bin/env python
 
+__author__ = "Iván Uría"
+
+"""This module gives persistency default functions.
+Functions:
+    persistent: Creates Fields and Entity objects for __entities and __fields tables
+    get_entity: Creates and returns an Entity object from table name
+    get_entities: Creates and returns all Entity object from database
+    install_persistency: Creates tables __entities and __fields in database
+"""
+
+import asyncio
 from .fields import Field, Fields
 from .entities import Entity
 from databases.databases import DBInterface, DBEnums
 from datetime import datetime
+from typing import NoReturn
 
-def persistent(database, *, loop=None):
+def persistent(database:DBInterface, *, loop:asyncio.BaseEventLoop=None) -> tuple:
+    """Returns a tuple with two Entitiy: the entity of entities and the entity of fields
+    Arguments:
+        database: DBInterface to play with
+    Key Arguments:
+        loop: asyncronous loop to give a real-time actualization. Optional.
+    """
     assert isinstance(database, DBInterface)
     ent_fields = Fields(database, "__entities",
                     {"name": str,
@@ -31,7 +49,20 @@ def persistent(database, *, loop=None):
                           loop=loop)
     return entity, fields_entity
 
-def get_entity(database, table, ent=None, *, loop=None):
+def get_entity(database:DBInterface, table:str, ent:dict=None, *, loop:asyncio.BaseEventLoop=None) -> Entity:
+    """Returns an Entity instance of the indicated table.
+    Arguments:
+        database: DBInterface to play with
+        table: name of the table
+        ent: dictionary of data to instantiate new Entity if needed. Optional
+            {"table_name": str,
+             "name": str,
+             "description": str,
+             "parent": str,
+             "parent_field": str}
+    Key Arguments:
+        loop: asyncronous loop to give a real-time actualization. Optional.
+    """
     if database in Entity.persistent and table in Entity.persistent[database]:
         return Entity.persistent[database][table]
     entity, fields_entity = persistent(database)
@@ -45,7 +76,13 @@ def get_entity(database, table, ent=None, *, loop=None):
     else:
         return None
 
-def get_entities(database, *, loop=None):
+def get_entities(database:DBInterface, *, loop:asyncio.BaseEventLoop=None) -> list:
+    """Returns a list of entities from database got from get_entity in a for loop
+    Arguments:
+        database: DBInterface to play with
+    Key Arguments:
+        loop: asyncronous loop to give a real-time actualization. Optional.
+    """
     entity, fields_entity = persistent(database, loop=loop)
     entities_list = entity.get({})
     final = {}
@@ -53,7 +90,11 @@ def get_entities(database, *, loop=None):
         final[ent["name"]] = get_entity(database, ent["table_name"], ent=ent)
     return final
 
-def install_persistency(database):
+def install_persistency(database:DBInterface) -> NoReturn:
+    """Creates the tables __fields and __entities
+    Arguments:
+        database: DBInterface to play with
+    """
     entity, fields_entity = persistent(database)
     entity.install()
     fields_entity.install()
