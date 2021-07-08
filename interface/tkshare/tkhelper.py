f70b72b2529e227d744323ba1bbef25a8a98fd53
ivanuria <ivan.uria@gmail.com>
2021-07-08 17:06:45 +0200

    Created a helper file and an ItemWrapper.

---
+++ interface/tkshare/tkhelper.py
@@ -0,0 +1,37 @@
+"""Helper to set new classes for tkinter and make it easier.
+"""
+
+from datetime import datetime
+from entities.entities import Item
+from typing import NoReturn
+import tkinter as tk
+
+
+class ItemWrapper(dict):
+    """For wrapping an Item and returning tkinter types
+    It overrides dict making it mover easy to use.
+    Arguments:
+        item: Item to wrapc
+    """
+    def __init__(self, item:Item, config:dict) -> NoReturn:
+        super().__init__()
+        self.item = item
+        self._keys = list(item.entity.fields.keys())
+        self._types = list(item.entity.fields.values())
+        primary_index = self._keys.index(self.item.primary_key)
+        #Primaryindex is always a list [type, DBEnum.PRIMARY]
+        self._types[primary_index] = self._types[primary_index][0]
+        types = {
+        # It will be a tuple with three values: [tkinter, getter, setter]
+        # Both getter and setter would be lambdas for conversion if neccesary
+            int: [tk.IntVar, lambda x: x, lambda x: x],
+            str: [tk.StringVar, lambda x: x, lambda x: x],
+            datetime: [tk.StringVar, lambda x: x.strftime("dd-mm-YYYY HH:MM:SS"), lambda x: datetime.strptime(x, "dd-mm-YYYY HH:MM:SS")], #TODO: Make this configurable
+            float: [tk.DoubleVar, lambda x: x, lambda x: x],
+            bool: [tk.BooleanVar, lambda x: x, lambda x: x]
+        }
+        for i, key in enumerate(self._keys):
+            variable, getter, setter = types[self._types[i]]
+            variable.trace_add("w", lambda name, *_, key=key: self.item.changed_handler(key)(setter(tk.Variable(name).get())))
+            self.item.set_handler(key, lambda x: variable.set(getter(x)))
+            super().__setitem__(key, variable)
